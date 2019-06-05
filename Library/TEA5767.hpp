a5a5ee96a51cf855aa3b97cf8803c99300668f49
Jochem <jochem.vankranenburg@student.hu.nl>
2019-06-05 13:37:10 +0200

    Werkende frequentiewisseling

--- Library/TEA5767.hpp
+++ Library/TEA5767.hpp
@@ -3,7 +3,7 @@
 
 class TEA5767{
 	private:
-		hwlib::i2c_bus_bit_banged_scl_sda & bus;
+		hwlib::i2c_bus & bus;
 		uint8_t address;
 	public:
 		TEA5767(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address = 0x60);
