c1afd0ae4f02d4b8e26f954e9e91f7273082f704
Jochem <jochem.vankranenburg@student.hu.nl>
2019-06-07 17:19:36 +0200

    HiLo voorkeur toegevoegd

--- Library/TEA5767.cpp
+++ Library/TEA5767.cpp
@@ -1,9 +1,10 @@
 #include "hwlib.hpp"
 #include "TEA5767.hpp"
 
-TEA5767::TEA5767(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address):
+TEA5767::TEA5767(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address, int hiLoPrefrence):
 	bus(bus),
-	address(address)
+	address(address),
+	hiLoPrefrence(hiLoPrefrence)
 {}
 
 void TEA5767::setData(){
@@ -44,9 +45,17 @@ int TEA5767::testHiLo(float frequency){
 	}
 }
 
+void TEA5767::setPrefrence(int hilo){
+	hiLoPrefrence = hilo;
+}
+
 
 void TEA5767::setFrequency(float frequency){
-	setHiLo(frequency, testHiLo(frequency));
+	if(hiLoPrefrence == -1){
+		setHiLo(frequency, testHiLo(frequency));
+	} else {
+		setHiLo(frequency, hiLoPrefrence);
+	}
 }
 
 int TEA5767::signalStrength(){
--- Library/TEA5767.hpp
+++ Library/TEA5767.hpp
@@ -7,14 +7,16 @@ class TEA5767{
 		uint8_t address;
 		uint8_t data[5] = {0x30, 0x1F, 0x10, 0x10, 0xC0};
 		uint8_t status[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
+		int hiLoPrefrence;
 		void setData();
 		void getStatus();
 		int testHiLo(float frequency);
 		void setHiLo(float frequency, int hilo);
 	public:
-		TEA5767(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address = 0x60);
+		TEA5767(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address = 0x60, int hiLoPrefrence = -1);
 		void setFrequency(float frequency);
 		int signalStrength();
+		void setPrefrence(int hilo);
 };
 
 #endif //__TEA5767_HPP
