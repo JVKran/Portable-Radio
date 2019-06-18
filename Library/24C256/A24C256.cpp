e29de11dc167b34a3f63a432500924638239e85a
Jochem <jochem.vankranenburg@student.hu.nl>
2019-06-18 12:57:08 +0200

    Finalized Library For 24C EEPROM chips

--- Library/24C256/A24C256.cpp
+++ Library/24C256/A24C256.cpp
@@ -1,87 +1,110 @@
 #include "hwlib.hpp"
 #include "A24C256.hpp"
 
-A24C256::A24C256(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address):
+A24C256::A24C256(hwlib::i2c_bus_bit_banged_scl_sda & bus, unsigned int memorySize, uint8_t address):
 	bus(bus),
+	memorySize(memorySize*128),							//Verify how many addresses are available
 	address(address)
 {}
 
-void A24C256::writeByte(unsigned int location, uint8_t value){
-	if(location >= 0 && location <= 32767){
-		data[0] = location >> 8;
-		data[1] = location & 0xFF;
-		data[2] = value;
+void A24C256::write(unsigned int location, uint8_t value){
+	if(location >= 0 && location < memorySize){
+		data[0] = location >> 8;						//MSB Location
+		data[1] = location & 0xFF;						//LSB Location
+		data[2] = value;								//Value to save
 		bus.write(address).write(data, 3);
 	}
-	hwlib::wait_ms(5); //Datasheet states it can take up to 5ms for the chip to become responsive again.
+	hwlib::wait_ms(5); 									//Datasheet states it can take up to 5ms for the chip to become responsive again.
 }
 
-uint8_t A24C256::readByte(unsigned int location){
-	if(location >= 0 && location <= 32767){
+uint8_t A24C256::read(unsigned int location){
+	if(location >= 0 && location < memorySize){
 		uint8_t receivedData;
 		data[0] = location >> 8;
 		data[1] = location & 0xFF;
-		bus.write(address).write(data, 2);
-		bus.read(address).read(&receivedData, 1);
+		bus.write(address).write(data, 2);				//Tell which address to get data from
+		bus.read(address).read(&receivedData, 1);		//Retrieve data
 		return receivedData;
 	} else {
-		return -1;
+		return 0;
 	}
 }
 
 void A24C256::write(unsigned int location, char* value, bool largeBuffer){
-	unsigned int pageSize;
-	if (largeBuffer) {
-		pageSize = 64;
-	} else {
-		pageSize = 32;
-	}
-	unsigned int length = 0;
-	while(value[length]){
-		length++;
-	}
-	if(length + (location % pageSize) <= pageSize-1){
-		data[0] = location >> 8;
-		data[1] = location & 0xFF;
-		for(unsigned int i = 0; i < length; i++){
-			data[i+2] = uint8_t(value[i]);
-		}
-		bus.write(address).write(data, length+2);
-		hwlib::wait_ms(5);
-	} else {
-		unsigned int cycles = 0;
-		if(float((float(length) + (float(location % pageSize)) / pageSize) - length) > 0.0){
-			cycles = ((length + (location % pageSize)) / pageSize)  + 1;
+	if(location >= 0 && location < memorySize){
+		unsigned int pageSize;
+		/*						Buffer-Size
+		A Buffer-size of 64 bytes is faster and more efficient.
+		Though not all microcontrollers or I2C libraries can handle this.
+		To make this library user-friendly you can choose which one to use.
+		*/
+		if (largeBuffer) {
+			pageSize = 64;
 		} else {
-			cycles = (length + (location % pageSize)) / pageSize;
+			pageSize = 32;
 		}
-		for(unsigned int i = 0; i <= cycles; i++){
-			data[0] = location >> 8;
-			data[1] = location & 0xFF;
-			if(cycles > i){
-				for(unsigned int j = 0; j <= pageSize; j++){
-					data[j+2] = uint8_t(value[j+(i * pageSize)]);
+		unsigned int length = 0;
+		while(value[length]){
+			length++;
+		}
+		/*						Pages
+		The memory consists of pages of 64 bytes each. When an
+		edge of a page is reached, the write process will continue
+		at the beginning of this current page; thus overwriting data.
+		To prevent that from happening, we monitor when this will happen.
+		We'll write multiple bytes of amount 'pageSize' or as much that
+		will fit in the current page. If there is data left, we'll write
+		that to the next page.
+		*/
+		if(length + location < memorySize){						//Check whether all data will actually fit whithout overwriting data at address 0,1,...
+			if(length + (location % pageSize) < pageSize){		//If the date doesn't cross a page-boundary
+				data[0] = location >> 8;
+				data[1] = location & 0xFF;
+				for(unsigned int i = 0; i < length; i++){
+					data[i+2] = uint8_t(value[i]);
 				}
-				location+=pageSize;
-				bus.write(address).write(data, pageSize + 2);
+				bus.write(address).write(data, length+2);
 				hwlib::wait_ms(5);
 			} else {
-				for(unsigned int j = 0; j < length % pageSize; j++){
-					data[j+2] = uint8_t(value[j+(cycles * pageSize)]);
+				unsigned int cycles = 0;
+				if(float((float(length) + (float(location % pageSize)) / pageSize) - length) > 0.0){		//If there is data left to save;
+					cycles = ((length + (location % pageSize)) / pageSize)  + 1;							//add another write-cycle
+				} else {
+					cycles = (length + (location % pageSize)) / pageSize;
 				}
-				location += length % pageSize;
-				bus.write(address).write(data, (length % pageSize + 2));
-				hwlib::wait_ms(5);
+				for(unsigned int i = 0; i <= cycles; i++){
+					data[0] = location >> 8;												//Update location from where we can start again; past the page-boundary
+					data[1] = location & 0xFF;
+					if(cycles > i){
+						for(unsigned int j = 0; j <= pageSize; j++){						//Put the max amount of data in buffer until page-boundary is reached
+							data[j+2] = uint8_t(value[j+(i * pageSize)]);					// Reminder: first two bytes form the beginning location to save the data
+						}
+						location+=pageSize;
+						bus.write(address).write(data, pageSize + 2);
+						hwlib::wait_ms(5);
+					} else {																//If last cycle is reached we only need to write the left-over bytes;
+						for(unsigned int j = 0; j < length % pageSize; j++){				//amount % 64 = amount to write in last cycle
+							data[j+2] = uint8_t(value[j+(cycles * pageSize)]);
+						}
+						location += length % pageSize;
+						bus.write(address).write(data, (length % pageSize + 2));
+						hwlib::wait_ms(5);
+					}
+				}
 			}
+			hwlib::wait_ms(5);
 		}
 	}
-	hwlib::wait_ms(5);
 }
 
 uint8_t A24C256::read(unsigned int location, unsigned int length, uint8_t receivedData[]){
-	data[0] = location >> 8;
-	data[1] = location & 0xFF;
-	bus.write(address).write(data, 2);
-	bus.read(address).read(receivedData, length);
-	return *receivedData;
+	if(location >= 0 && location < memorySize){
+		data[0] = location >> 8;
+		data[1] = location & 0xFF;
+		bus.write(address).write(data, 2);
+		bus.read(address).read(receivedData, length);				//Read multiple bytes at once
+		return *receivedData;
+	} else {
+		return 0;
+	}
 }
--- Library/24C256/A24C256.hpp
+++ Library/24C256/A24C256.hpp
@@ -4,13 +4,14 @@
 class A24C256{
 	private:
 		hwlib::i2c_bus & bus;
+		unsigned int memorySize;	//This library is also compatible with 24C24C32, 24C64, 24C65, 24C128, 24C256, 24C512, 24C1024
 		const uint8_t address;
-		uint8_t data[65] = {};
+		uint8_t data[65] = {};		//Two address bytes followed by data to save
 	public:
-		A24C256(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address = 0x50);
-		void writeByte(unsigned int location, uint8_t value);
+		A24C256(hwlib::i2c_bus_bit_banged_scl_sda & bus, unsigned int memorySize = 256, uint8_t address = 0x50);
+		void write(unsigned int location, uint8_t value);
 		void write(unsigned int location, char* value, bool largeBuffer = true); 		//Should be 32 or 64
-		uint8_t readByte(unsigned int location);
+		uint8_t read(unsigned int location);
 		uint8_t read(unsigned int location, unsigned int length, uint8_t receivedData[] = {});
 };
 
