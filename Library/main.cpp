58aaab6fefb80f1dfdac1823a0678d77f09e917d
Jochem <jochem.vankranenburg@student.hu.nl>
2019-06-18 11:29:30 +0200

    Changed File Structure

--- Library/main.cpp
+++ Library/main.cpp
@@ -4,8 +4,8 @@
 int main( void ){
     namespace target = hwlib::target; 
 
-    auto scl = target::pin_oc( target::pins::d8 );
-    auto sda = target::pin_oc( target::pins::d9 );
+	auto scl = target::pin_oc( target::pins::d8 );
+	auto sda = target::pin_oc( target::pins::d9 );
 	auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
 
 	auto memory = A24C256(i2c_bus);
