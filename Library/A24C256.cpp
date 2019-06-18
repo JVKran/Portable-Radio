65a19f3341eec9b97f9ce3766f1f8ed21a37bbf3
Jochem <jochem.vankranenburg@student.hu.nl>
2019-06-18 10:51:56 +0200

    Tiedied Up

--- Library/A24C256.cpp
+++ Library/A24C256.cpp
@@ -29,12 +29,18 @@ uint8_t A24C256::readByte(unsigned int location){
 	}
 }
 
-void A24C256::write(unsigned int location, char* value){
+void A24C256::write(unsigned int location, char* value, bool largeBuffer){
+	unsigned int pageSize;
+	if (largeBuffer) {
+		pageSize = 64;
+	} else {
+		pageSize = 32;
+	}
 	unsigned int length = 0;
 	while(value[length]){
 		length++;
 	}
-	if(length + (location % 32) <= 29){
+	if(length + (location % pageSize) <= pageSize-1){
 		data[0] = location >> 8;
 		data[1] = location & 0xFF;
 		for(unsigned int i = 0; i < length; i++){
@@ -44,33 +50,27 @@ void A24C256::write(unsigned int location, char* value){
 		hwlib::wait_ms(5);
 	} else {
 		unsigned int cycles = 0;
-		unsigned int start = 0;
-		if(float((float(length) + (float(location % 32)) / 32) - length) > 0.0){
-			cycles = ((length + (location % 32)) / 32)  + 1;
+		if(float((float(length) + (float(location % pageSize)) / pageSize) - length) > 0.0){
+			cycles = ((length + (location % pageSize)) / pageSize)  + 1;
 		} else {
-			cycles = (length + (location % 32)) / 32;
+			cycles = (length + (location % pageSize)) / pageSize;
 		}
 		for(unsigned int i = 0; i <= cycles; i++){
 			data[0] = location >> 8;
 			data[1] = location & 0xFF;
 			if(cycles > i){
-				if(i == 0){
-					start = 0;
-				} else {
-					start = 1;
+				for(unsigned int j = 0; j <= pageSize; j++){
+					data[j+2] = uint8_t(value[j+(i * pageSize)]);
 				}
-				for(unsigned int j = 0; j <= 32; j++){
-					data[j+2] = uint8_t(value[j+(i * 32)]);
-				}
-				location+=32;
-				bus.write(address).write(data, 32);
+				location+=pageSize;
+				bus.write(address).write(data, pageSize);
 				hwlib::wait_ms(5);
 			} else {
-				for(unsigned int j = 1; j <= length % 32; j++){
-					data[j+2] = uint8_t(value[j+(cycles * 32)]);
+				for(unsigned int j = 1; j <= length % pageSize; j++){
+					data[j+2] = uint8_t(value[j+(cycles * pageSize)]);
 				}
-				location += length % 33;
-				bus.write(address).write(data, (length % 32 + 2));
+				location += length % pageSize;
+				bus.write(address).write(data, (length % pageSize + 2));
 				hwlib::wait_ms(5);
 			}
 		}
@@ -78,3 +78,9 @@ void A24C256::write(unsigned int location, char* value){
 	hwlib::wait_ms(5);
 }
 
+/*
+char* A24C256::read(unsigned int location, unsigned int length){
+	data[0] = location >> 8;
+	data[1] = location & 0xFF;
+}
+*/
--- Library/A24C256.hpp
+++ Library/A24C256.hpp
@@ -5,12 +5,13 @@ class A24C256{
 	private:
 		hwlib::i2c_bus & bus;
 		const uint8_t address;
-		uint8_t data[33] = {};
+		uint8_t data[65] = {};
 	public:
 		A24C256(hwlib::i2c_bus_bit_banged_scl_sda & bus, uint8_t address = 0x50);
 		void writeByte(unsigned int location, uint8_t value);
-		void write(unsigned int location, char* value);
+		void write(unsigned int location, char* value, bool largeBuffer = true); 		//Should be 32 or 64
 		uint8_t readByte(unsigned int location);
+		//char* read(unsigned int location, unsigned int length);
 };
 
 #endif //__A24C256_HPP
