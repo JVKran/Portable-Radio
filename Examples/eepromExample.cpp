6a3a6c63799ae67d0c8aea1cb7286d9d3dd2e652
Jochem <jochem.vankranenburg@student.hu.nl>
2019-07-03 20:02:16 +0200

    Checked Examples

--- Examples/eepromExample.cpp
+++ Examples/eepromExample.cpp
@@ -6,63 +6,63 @@ int main( void ){
 
     auto scl = target::pin_oc( target::pins::d8 );
     auto sda = target::pin_oc( target::pins::d9 );
-	auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
+  auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
 
-	auto memory = A24C256(i2c_bus);
-	auto largeMemory = A24C256(i2c_bus, 512);
-	auto repairedMemory = A24C256(i2c_bus, 1000); 	//Is automatically resized to 1024.
+  auto memory = A24C256(i2c_bus);
+  auto largeMemory = A24C256(i2c_bus, 512);
+  auto repairedMemory = A24C256(i2c_bus, 1000);   //Is automatically resized to 1024.
 
-	//Write one single byte at location 0 and retrieve it
-	memory.write(0, 'c');
-	hwlib::cout << char(memory.read(0)) << hwlib::endl;
+  //Write one single byte at location 0 and retrieve it
+  memory.write(0, 'c');
+  hwlib::cout << char(memory.read(0)) << hwlib::endl;
 
-	//Write 12 bytes to location 20
-	char data[]={"Hello World!"};
-	memory.write(20, data);
+  //Write 12 bytes to location 20
+  char data[]={"Hello World!"};
+  memory.write(20, data);
 
-	//Retrieve multiple bytes (efficient way)
-	uint8_t receivedData[12];
-	memory.read(20, 12, receivedData);
+  //Retrieve multiple bytes (efficient way)
+  uint8_t receivedData[12];
+  memory.read(20, 12, receivedData);
 
-	//Print received bytes
-	for(unsigned int i = 0; i < 12; i++){
-		hwlib::cout << char(receivedData[i]);
-	}
-	hwlib::cout << hwlib::endl;
+  //Print received bytes
+  for(unsigned int i = 0; i < 12; i++){
+    hwlib::cout << char(receivedData[i]);
+  }
+  hwlib::cout << hwlib::endl;
 
-	//Another (less efficient) way to retrieve multiple bytes
-	for(unsigned int i = 20; i < 32; i++){
-		hwlib::cout << char(memory.read(i));
-	}
-	hwlib::cout << hwlib::endl;
+  //Another (less efficient) way to retrieve multiple bytes
+  for(unsigned int i = 20; i < 32; i++){
+    hwlib::cout << char(memory.read(i));
+  }
+  hwlib::cout << hwlib::endl;
 
-	//Write integer 100 to address 0 (since a uint8_t is written, the max size of the integer is 255)
-	memory.write(0, 100);
-	hwlib::cout << memory.read(0) << hwlib::endl;
+  //Write integer 100 to address 0 (since a uint8_t is written, the max size of the integer is 255)
+  memory.write(0, 100);
+  hwlib::cout << memory.read(0) << hwlib::endl;
 
-	//Way to store a boolean
-	memory.write(0, true);
-	hwlib::cout << (bool(memory.read(0)) && 1);
+  //Way to store a boolean
+  memory.write(0, true);
+  hwlib::cout << (bool(memory.read(0)) && 1);
 
-	//Overwriting page-boundaries is taken care of.
-	char data[]={"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio."};
-	memory.write(0, data);
+  //Overwriting page-boundaries is taken care of.
+  char newData[]={"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio."};
+  memory.write(0, newData);
 
-	uint8_t receivedData[353];
-	memory.read(0, 353, receivedData);
+  uint8_t newReceivedData[353];
+  memory.read(0, 353, newReceivedData);
 
-	hwlib::cout << hwlib::endl << "Saved Text: " << hwlib::endl;
+  hwlib::cout << hwlib::endl << "Saved Text: " << hwlib::endl;
 
-	for(unsigned int i = 0; i < 353; i++){
-		hwlib::cout << char(receivedData[i]);
-	}
+  for(unsigned int i = 0; i < 353; i++){
+    hwlib::cout << char(newReceivedData[i]);
+  }
 
-	//Making pin WP high, disables its functionality to save given data; it protects saved data.
-	memory.write(300, 'c');
-	memory.setWriteProtect();
-	hwlib::cout << hwlib::setw(100) << hwlib::left << "Write-Protect Enabled: " << memory.getWriteProtect() << hwlib::endl;
-	memory.write(300, 'z');
-	hwlib::cout << hwlib::setw(100) << hwlib::left << "Writing impossible when Write Protection Enabled " << hwlib::boolalpha << (char(memory.read(300)) == 'c') << hwlib::endl;
+  //Making pin WP high, disables its functionality to save given data; it protects saved data.
+  memory.write(300, 'c');
+  memory.setWriteProtect();
+  hwlib::cout << hwlib::setw(100) << hwlib::left << "Write-Protect Enabled: " << memory.getWriteProtect() << hwlib::endl;
+  memory.write(300, 'z');
+  hwlib::cout << hwlib::setw(100) << hwlib::left << "Writing impossible when Write Protection Enabled " << hwlib::boolalpha << (char(memory.read(300)) == 'c') << hwlib::endl;
 
 
 }
--- Library/main.cpp
+++ Library/main.cpp
@@ -1,147 +1,68 @@
-/// @file
-
 #include "hwlib.hpp"
-#include "DS3231.hpp"
+#include "A24C256.hpp"
 
-/// \brief
-/// Test
-/// \details
-/// This program tests ALL functionality of the DS3231 Real Time clock.
 int main( void ){
-  namespace target = hwlib::target;
+    namespace target = hwlib::target; 
 
-  auto scl = target::pin_oc( target::pins::d8 );
-  auto sda = target::pin_oc( target::pins::d9 );
+    auto scl = target::pin_oc( target::pins::d8 );
+    auto sda = target::pin_oc( target::pins::d9 );
   auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
 
-  auto clock = DS3231(i2c_bus);
+  auto memory = A24C256(i2c_bus);
+  auto largeMemory = A24C256(i2c_bus, 512);
+  auto repairedMemory = A24C256(i2c_bus, 1000);   //Is automatically resized to 1024.
 
-  auto time = timeData(15, 20, 10);
-  auto lastTime = timeData(5, 30);
-  auto bigTime = timeData(50, 60, 80);
+  //Write one single byte at location 0 and retrieve it
+  memory.write(0, 'c');
+  hwlib::cout << char(memory.read(0)) << hwlib::endl;
 
-  hwlib::wait_ms(1000);
+  //Write 12 bytes to location 20
+  char data[]={"Hello World!"};
+  memory.write(20, data);
 
-  hwlib::cout << "---------------------------------TIME------------------------------" << hwlib::endl << hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::boolalpha << hwlib::setw(45) << "Initialization: " << ((time.getHours() == 15) && time.getMinutes() == 20 && time.getSeconds() == 10) << hwlib::endl;
+  //Retrieve multiple bytes (efficient way)
+  uint8_t receivedData[12];
+  memory.read(20, 12, receivedData);
 
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Too Large Initialization Handling: " << ((bigTime.getHours() == 0) && bigTime.getMinutes() == 0 && bigTime.getSeconds() == 0) << hwlib::endl;
-
-  time.setTime(11, 40, 55);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Setting: " << ((time.getHours() == 11) && time.getMinutes() == 40 && time.getSeconds() == 55) << hwlib::endl;
-
-  time.setTime(80, 90, 100);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Too Large Setting Handling: " << (time == timeData(0, 0, 0)) << hwlib::endl;
-
-  time.setTime(10, 30);
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Addition: " << time << " + " << lastTime << " = " << (time + lastTime) <<  hwlib::boolalpha << " " << ((time + lastTime) == timeData(16, 0, 0)) <<  hwlib::endl;
-  lastTime.setHours(18);
-  lastTime.setMinutes(15);
-  lastTime.setSeconds(55);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Addition: " << time << " + " << lastTime << " = " << (time + lastTime) <<  hwlib::boolalpha << " " << ((time + lastTime) == timeData(4, 45, 55)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Addition: " << timeData(0, 0) << " + " << timeData(0, 0) << " = " << (timeData(0+0, 0+0)) << " true" << hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Addition: " << timeData(23, 59) << " + " << timeData(23, 59) << " = " << (timeData(23, 59) + timeData(23, 59)) << " true" <<  hwlib::endl;
-
-  lastTime.setTime(5, 30);
-  time.setTime(10, 30);
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Substraction: " << time << " - " << lastTime << " = " << (time - lastTime) << hwlib::boolalpha << "  " << ((time - lastTime) == timeData(5, 0, 0)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << lastTime << " - " << time << " = " << (lastTime - time) << hwlib::boolalpha << " " << ((lastTime - time) == timeData(19, 0, 0)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << timeData(23, 59) << " - " << timeData(23, 59) << " = " << (timeData(23, 59) - timeData(23, 59)) << " true" << hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << timeData(0, 15) << " - " << timeData(0, 30) << " = " << (timeData(0, 15) - timeData(0, 30)) << " true" <<  hwlib::endl;
-
-  lastTime.setTime(5, 30);
-  time.setTime(10, 30);
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Comparison: " << time << " < " << lastTime << " = " << (time < lastTime) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << timeData(11, 30) << " < " << timeData(11, 30, 10) << " = " << (timeData(11, 30) < timeData(11, 30, 10)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << time << " <= " << lastTime << " = " << (time <= lastTime) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << time << " > " << lastTime << " = " << (time > lastTime) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << time << " >= " << lastTime << " = " << (time >= lastTime) <<  hwlib::endl;
-
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Equality: " << time << " == " << lastTime << " = " << (time == lastTime) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Unequality: " << lastTime << " != " << time << " = " << (lastTime != time) <<  hwlib::endl << hwlib::endl;
-
-  hwlib::cout << "---------------------------------DATE------------------------------" << hwlib::endl << hwlib::endl;
-
-  auto date = dateData(7, 30, 6, 2019);
-  auto lastDate = dateData(10, 35, 14, 2019);
-
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Initialization: " << (date.getWeekDay() == 7 && date.getMonthDay() == 30 && date.getMonth() == 6 && date.getYear() == 2019) << hwlib::endl;
-
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Too Large Initialization Handling: " << (lastDate.getWeekDay() == 1 && lastDate.getMonthDay() == 1 && lastDate.getMonth() == 1 && lastDate.getYear() == 2019) << hwlib::endl;
-
-  lastDate.setDate(7, 30, 6, 2019);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Setting: " << (lastDate.getWeekDay() == 7 && lastDate.getMonthDay() == 30 && lastDate.getMonth() == 6 && lastDate.getYear() == 2019) << hwlib::endl;
-
-  lastDate.setDate(80, 90, 100, 2019);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Too Large Setting Handling: " << (lastDate.getWeekDay() == 1 && lastDate.getMonthDay() == 1 && lastDate.getMonth() == 1 && lastDate.getYear() == 2019) << hwlib::endl;
-
-  date.setDate(1, 1, 1, 2019);
-  lastDate.setDate(7, 30, 6, 0);
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Addition: " << date.getWeekDay() << ", " << date << " + " << lastDate.getWeekDay() << ", " << lastDate << " = " << (date + lastDate) <<  hwlib::boolalpha << " " << ((date + lastDate) == dateData(1, 1, 8, 2019)) <<  hwlib::endl;
-
-  lastDate.setWeekDay(5);
-  lastDate.setMonthDay(15);
-  lastDate.setMonth(8);
-  lastDate.setYear(2000);
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Setting and Addition: " << date.getWeekDay() << ", " << date << " + " << lastDate.getWeekDay() << ", " << lastDate << " = " << (date + lastDate) <<  hwlib::boolalpha << " " << ((date + lastDate) == dateData(6, 16, 9, 4019)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Addition: " << dateData(6, 20, 12, 2019) << " + " << dateData(6, 20, 5, 0) << " = " << (dateData(6, 20, 5, 0) +  dateData(6, 20, 12, 2019)) <<  hwlib::boolalpha << " true" <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Addition: " << dateData(1, 1, 1, 2000) << " + " << dateData(1, 1, 1, 0) << " = " << (dateData(1, 1, 1, 2000) +  dateData(1, 1, 1, 0)) <<  hwlib::boolalpha << " true" <<  hwlib::endl;
-
-  date.setDate(5, 8, 10, 2019);
-  lastDate.setDate(4, 30, 8, 2000);
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Substraction: " << date.getWeekDay() << ", " << date << " - " << lastDate.getWeekDay() << ", " << lastDate << " = " << (date - lastDate) << hwlib::boolalpha << "  " << ((date - lastDate) == dateData(1, 8, 1, 19)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << lastDate.getWeekDay() << ", " << lastDate << " - " << date.getWeekDay() << ", " << date << " = " << (lastDate - date) << hwlib::boolalpha << " " << ((lastDate - date) == dateData(6, 22, 10, 0)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << dateData(6, 20, 12, 2019) << " - " << dateData(6, 20, 5, 0) << " = " << (dateData(6, 20, 12, 0) -  dateData(6, 20, 5, 2019)) <<  hwlib::boolalpha << " true" <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Substraction: " << dateData(6, 1, 1, 2019) << " - " << dateData(6, 6, 6, 0) << " = " << (dateData(6, 1, 1, 2019) -  dateData(6, 6, 6, 0)) <<  hwlib::boolalpha << " true" <<  hwlib::endl;
-
-  lastTime.setTime(5, 30);
-  time.setTime(10, 30);
-  
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Comparison: " << date << " < " << lastDate << " = " << (date < lastDate) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << date << " <= " << lastDate << " = " << (date <= lastDate) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << dateData(1, 1, 12, 2019) << " <= " << dateData(1, 1, 12, 2019) << " = " << (dateData(1, 1, 12, 2019) <= dateData(1, 1, 12, 2019)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << date << " > " << lastDate << " = " << (date > lastDate) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << dateData(1, 1, 12, 2019) << " > " << dateData(1, 1, 12, 2019) << " = " << (dateData(1, 1, 12, 2019) > dateData(1, 1, 12, 2019)) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Comparison: " << date << " >= " << lastDate << " = " << (date >= lastDate) <<  hwlib::endl;
-
-  hwlib::cout << hwlib::endl << hwlib::left << hwlib::setw(45) << "Equality: " << date.getWeekDay() << ", " << date << " == " << lastDate.getWeekDay() << ", " << lastDate << " = " << (date == lastDate) <<  hwlib::endl;
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Unequality: " << date.getWeekDay() << ", " << date << " != " << lastDate.getWeekDay() << ", " << lastDate << " = " << (date != lastDate) <<  hwlib::endl << hwlib::endl;
-
-
-  hwlib::cout << "-------------------------------DS3231--------------------------------" << hwlib::endl << hwlib::endl;
-
-  hwlib::cout << hwlib::left << hwlib::setw(45) << "Initialization: " << (clock.getAddress() == 0x68) << hwlib::endl;
-
-  hwlib::cout << hwlib::endl;
-  for(unsigned int i = 0; i < 3; i++){
-    time = clock.getTime();
-    date = clock.getDate();
-    hwlib::cout << "Time: " << clock.getTime() << hwlib::endl;
-    hwlib::cout << "Temperature: " << int(clock.getTemperature() * 10) << hwlib::endl;
-    hwlib::cout << "Date: " << clock.getDate() << hwlib::endl << hwlib::endl;
-    hwlib::wait_ms(3000);
+  //Print received bytes
+  for(unsigned int i = 0; i < 12; i++){
+    hwlib::cout << char(receivedData[i]);
   }
   hwlib::cout << hwlib::endl;
-  
-  //Uncomment if time is allowed to get lost. You'll have to set it again later.
-  //hwlib::cout << hwlib::left << hwlib::setw(45) << "Set time to 0:0:0 : ";
-  //clock.setTime(0, 0, 0);
-  //hwlib::cout << (clock.getTime() == timeData(0, 0, 0)) << hwlib::endl;
 
-  //The alarm other than the one you want to change and activate first has to be set; puts user more in control.
-  clock.setSecondAlarm(16);
+  //Another (less efficient) way to retrieve multiple bytes
+  for(unsigned int i = 20; i < 32; i++){
+    hwlib::cout << char(memory.read(i));
+  }
+  hwlib::cout << hwlib::endl;
 
-  auto curTime = clock.getTime();
-  curTime.setSeconds(curTime.getSeconds() + 10);
-  clock.changeFirstAlarm(curTime, dateData(0, 0, 1, 2019));
-  clock.setFirstAlarm(14);
-  hwlib::cout << "Alarm set, should go in 10 seconds: ";
+  //Write integer 100 to address 0 (since a uint8_t is written, the max size of the integer is 255)
+  memory.write(0, 100);
+  hwlib::cout << memory.read(0) << hwlib::endl;
 
+  //Way to store a boolean
+  memory.write(0, true);
+  hwlib::cout << (bool(memory.read(0)) && 1);
 
-  while(clock.checkAlarms() == 0){
-    hwlib::wait_ms(200);
+  //Overwriting page-boundaries is taken care of.
+  char newData[]={"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio."};
+  memory.write(0, newData);
+
+  uint8_t newReceivedData[353];
+  memory.read(0, 353, newReceivedData);
+
+  hwlib::cout << hwlib::endl << "Saved Text: " << hwlib::endl;
+
+  for(unsigned int i = 0; i < 353; i++){
+    hwlib::cout << char(newReceivedData[i]);
   }
 
-  hwlib::cout << "Triggered!" << hwlib::endl;
+  //Making pin WP high, disables its functionality to save given data; it protects saved data.
+  memory.write(300, 'c');
+  memory.setWriteProtect();
+  hwlib::cout << hwlib::setw(100) << hwlib::left << "Write-Protect Enabled: " << memory.getWriteProtect() << hwlib::endl;
+  memory.write(300, 'z');
+  hwlib::cout << hwlib::setw(100) << hwlib::left << "Writing impossible when Write Protection Enabled " << hwlib::boolalpha << (char(memory.read(300)) == 'c') << hwlib::endl;
+
 
 }
